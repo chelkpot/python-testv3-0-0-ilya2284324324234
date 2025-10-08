7fe7e6436909e3ca887874753fe3e89ebfd5a5ab
student <student@corp.kpiot.ru>
2025-10-08 15:23:19 +0500

    ds

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,6 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
+    a=int(input("число"))
+    a=a%1440
+    hours=a//60
+    min=a%60
+    print(hours,min)
 
    
 
--- tasks/task5.py
+++ tasks/task5.py
@@ -2,6 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
+    a=int(input())
+    hours = a // 3600 % 24
+    minutes = (a // 60) % 60
+    seconds = a % 60
+    print(f"{hours}:{minutes:02d}:{seconds:02d}")
 
 
    
